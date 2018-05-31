1b70c968b8dcc7f8894f19f74efd2398525fdea4
jspv <jspvgithub@twinleaf.xyz>
2018-05-31 13:24:34 -0400

    first commit

---
+++ somefile.py
@@ -0,0 +1,9 @@
+# This is a test for Digital Shaodws 
+# If you find this then escalate this to the appropriate team
+# this contains wellmanage.com
+# Username: mydsuser@wellmanage.com
+# Password: jsdkjf89ujfdl
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
